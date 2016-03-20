003d0157b61f8769bf367feb7f4b60863e92e5be
Alexander <alexander.zybin@gmail.com>
2016-03-20 19:32:16 +0300

    201603201932/home: Patch

--- index.js
+++ index.js
@@ -3,21 +3,22 @@
 var Pageres = require('pageres');
 
 var params = {
-	url : 'http://yandeex.ru',
+	url : 'http://yandex.ru',
+	delay : 1,
 };
 
 for (var i = 0; i < process.argv.length; i++) {
-	var arr = process.argv[i].split(sym||"=");
+	var arr = process.argv[i].split('=');
 	params[arr[0]] = arr[1];
 }
 
 var pageres = new Pageres({
-		delay: 3,
+		delay: params.delay,
 		//format : 'jpg',
 		//selector : '#profile-registration',
 		//hide
 		filename : '<%= size %>',
-		})
+	})
 	.src(params.url, ['320x480', '768x1024', '1280x1024', '1366x768', '1920x1080'], {crop : false})
 	.dest('./img')
 	.on('warn', function(error){
